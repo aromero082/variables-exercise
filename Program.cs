1da2c9b5b979699a8132e8aefee530848742d489
Aaron <git@github.com:aromero082/PizzaAlgo.git>
2021-10-21 20:23:06 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PlayingWithVariablesConsle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { // Variable Declerations
+            string name;
+            int age;
+            char cars;
+            bool isAlive;
+            double weight;
+            decimal savings;
+
+            // Variable Initalizations
+
+            name = "Aaron";
+            age = 38;
+            cars = '2';
+            isAlive = true;
+            weight = 160;
+            savings = 200.20m;
+
+        }
+    }
+}
